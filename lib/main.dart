bf20bdadc45e17348224090921e57552201af877
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2025-12-23 15:54:57 +0500

    feat(main): enhance main function with logging and date formatting initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:l/l.dart';
+import 'package:stock_investment_app/app/di/app_scope.dart';
 import 'package:stock_investment_app/pages/stocks/stocks_screen.dart';
 import 'package:utils/utils_dart.dart';
+import 'package:utils/utils_flutter.dart';
+import 'package:yx_scope_flutter/yx_scope_flutter.dart';
 
 Future<void> main() async {
+  await l.capture(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+
+      await initializeDateFormatting('kk');
+
+      final appScopeHolder = AppScopeHolder();
+      await appScopeHolder.create();
+
+      runApp(ScopeProvider(holder: appScopeHolder, child: const MyApp()));
+    },
+    LogOptions(
+      handlePrint: true,
+      printColors: true,
+      outputInRelease: false,
+      output: LogOutput.developer,
+      messageFormatting: LConfig.messageFormatting,
+    ),
+  );
+
   runApp(const MyApp());
 }
 
